94f700d8c33bc238e768f47ccc34e9d7e4f6e5fe
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-17 13:08:16 -0600

    JSONTest

--- src/test/java/com/API/springboot/restapi/survey/SurveyResourceIT.java
+++ src/test/java/com/API/springboot/restapi/survey/SurveyResourceIT.java
@@ -1,6 +1,8 @@
 package com.API.springboot.restapi.survey;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -31,14 +33,16 @@ public class SurveyResourceIT {
     private TestRestTemplate template;
 
     @Test
-     void  retrieveSpecificSurveyQuestion_basicScenario(){
+     void  retrieveSpecificSurveyQuestion_basicScenario() throws JSONException {
         ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
         String expectedResponse =
                 """
                 {"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
                         """;
-        assertEquals(expectedResponse.trim(), responseEntity.getBody());
-        System.out.println(responseEntity.getBody());
-        System.out.println(responseEntity.getHeaders());
+
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(),false);
+//        assertEquals(expectedResponse.trim(), responseEntity.getBody());
+//        System.out.println(responseEntity.getBody());
+//        System.out.println(responseEntity.getHeaders());
     }
 }
