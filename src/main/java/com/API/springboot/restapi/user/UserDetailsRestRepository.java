ded12ea9517ee6c3d5aaa120a0225e9c9e7c7bb3
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-17 12:46:51 -0600

    jsonassert

---
+++ src/main/java/com/API/springboot/restapi/user/UserDetailsRestRepository.java
@@ -0,0 +1,9 @@
+package com.API.springboot.restapi.user;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import java.util.List;
+
+public interface UserDetailsRestRepository extends PagingAndSortingRepository<UserDetails, Long> {
+   List<UserDetails> findByRole(String role);
+
+}
